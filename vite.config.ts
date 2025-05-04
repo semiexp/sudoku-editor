85465d8a217fbb6a3b60ac894a1bfc6ba5265fed
semiexp <linthya.v@gmail.com>
2025-05-04 14:24:48 +0900

    change base dir and file names

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
+  worker: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      }
+    }
+  },
 })
