f11480fefede6bf07ed6df4d01cfe3f182d674af
semiexp <linthya.v@gmail.com>
2025-05-06 10:35:25 +0900

    add more benchmarks

--- solver/src/bin/bench.rs
+++ solver/src/bin/bench.rs
@@ -1,4 +1,6 @@
-use sudoku_variants_solver::puzzle::{self, Blocks, GivenNumbers, NonConsecutive, Puzzle};
+use sudoku_variants_solver::puzzle::{
+    self, Arrow, Blocks, GivenNumbers, NonConsecutive, Pos, Puzzle, Skyscrapers, Thermo,
+};
 use sudoku_variants_solver::solver::{irrefutable_facts, SolverConfig};
 
 const CONFIGS: [(&'static str, SolverConfig); 3] = [
@@ -26,7 +28,7 @@ const CONFIGS: [(&'static str, SolverConfig); 3] = [
 ];
 
 fn show_header() {
-    print!("| {:20}", "Instance");
+    print!("| {:25}", "Instance");
     for (name, _) in CONFIGS.iter() {
         print!(" | {:8}", name);
     }
@@ -36,7 +38,7 @@ fn show_header() {
 fn run_bench(desc: &str, puzzle: Puzzle) {
     let mut expected_answer = None;
 
-    print!("| {:20}", desc);
+    print!("| {:25}", desc);
     for (_, config) in CONFIGS.iter() {
         let start = std::time::Instant::now();
         let answer = irrefutable_facts(&puzzle, *config);
@@ -89,6 +91,9 @@ fn given_numbers(data: &[[i32; 9]]) -> GivenNumbers {
 }
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let full = args.iter().any(|arg| arg == "--full");
+
     show_header();
 
     run_bench(
@@ -98,18 +103,6 @@ fn main() {
             blocks: Some(default_blocks(3)),
             given_numbers: puzzle::GivenNumbers {
                 numbers: vec![vec![None; 9]; 9],
-            },
-            ..Default::default()
-        },
-    );
-
-    run_bench(
-        "no_clue_16x16",
-        Puzzle {
-            size: 16,
-            blocks: Some(default_blocks(4)),
-            given_numbers: puzzle::GivenNumbers {
-                numbers: vec![vec![None; 16]; 16],
             },
             ..Default::default()
         },
@@ -176,44 +169,140 @@ fn main() {
     );
 
     run_bench(
-        "few_clues_noncon1",
+        "few_clues_arrow",
         Puzzle {
             size: 9,
             blocks: Some(default_blocks(3)),
             given_numbers: given_numbers(&[
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 2, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 4, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 8, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 6, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
             ]),
-            non_consecutive: Some(NonConsecutive {}),
+            arrow: Some(Arrow {
+                arrows: vec![vec![
+                    Pos { x: 3, y: 5 },
+                    Pos { x: 4, y: 5 },
+                    Pos { x: 5, y: 6 },
+                    Pos { x: 6, y: 7 },
+                ]],
+            }),
             ..Default::default()
         },
     );
 
     run_bench(
-        "few_clues_noncon2",
+        "few_clues_thermo",
         Puzzle {
             size: 9,
             blocks: Some(default_blocks(3)),
             given_numbers: given_numbers(&[
-                [1, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 3, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 2, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 4, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 4, 0, 0],
+                [0, 0, 0, 0, 0, 8, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 6, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
             ]),
-            non_consecutive: Some(NonConsecutive {}),
+            thermo: Some(Thermo {
+                thermos: vec![vec![
+                    Pos { x: 3, y: 5 },
+                    Pos { x: 4, y: 5 },
+                    Pos { x: 5, y: 6 },
+                    Pos { x: 6, y: 7 },
+                ]],
+            }),
             ..Default::default()
         },
     );
+
+    run_bench(
+        "few_clues_skyscrapers",
+        Puzzle {
+            size: 9,
+            blocks: Some(default_blocks(3)),
+            given_numbers: given_numbers(&[
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 5, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 3, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 6, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]),
+            skyscrapers: Some(Skyscrapers {
+                up: vec![None, None, None, None, None, None, None, Some(6), None],
+                down: vec![None, None, None, None, None, None, None, None, None],
+                left: vec![None, None, None, Some(5), None, None, None, None, None],
+                right: vec![None, None, None, None, None, Some(3), None, None, None],
+            }),
+            ..Default::default()
+        },
+    );
+
+    if full {
+        run_bench(
+            "no_clue_16x16",
+            Puzzle {
+                size: 16,
+                blocks: Some(default_blocks(4)),
+                given_numbers: puzzle::GivenNumbers {
+                    numbers: vec![vec![None; 16]; 16],
+                },
+                ..Default::default()
+            },
+        );
+
+        run_bench(
+            "few_clues_noncon1",
+            Puzzle {
+                size: 9,
+                blocks: Some(default_blocks(3)),
+                given_numbers: given_numbers(&[
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 8, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                ]),
+                non_consecutive: Some(NonConsecutive {}),
+                ..Default::default()
+            },
+        );
+
+        run_bench(
+            "few_clues_noncon2",
+            Puzzle {
+                size: 9,
+                blocks: Some(default_blocks(3)),
+                given_numbers: given_numbers(&[
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 3, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 4, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 6, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                ]),
+                non_consecutive: Some(NonConsecutive {}),
+                ..Default::default()
+            },
+        );
+    }
 }
